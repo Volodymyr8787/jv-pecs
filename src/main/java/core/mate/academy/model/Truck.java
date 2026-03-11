fede1446b63f28ef5985a95c279ce9c46b0237a0
Volodymyr <vtsapchuk2017@gmail.com>
2026-03-11 21:28:16 +0200

    create pecs

--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,11 +5,24 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private String name;
+    private String color;
+
     public Truck() {
     }
 
     @Override
     public void doWork() {
         System.out.println("Truck started to work");
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
     }
 }
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -1,7 +1,43 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.*;
+
+import java.util.List;
+
 /**
  * Your implementation of MachineService.
  */
-public class MachineServiceImpl {
+public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
+    @Override
+    public List<T> getAll(Class<? extends Machine> type) {
+        if (type == Bulldozer.class) {
+            MachineProducer<Bulldozer> producer = new BulldozerProducer();
+            return (List<T>) producer.get();
+        }
+        if (type == Truck.class) {
+            MachineProducer<Truck> producer = new TruckProducer();
+            return (List<T>) producer.get();
+        }
+        if (type == Excavator) {
+            MachineProducer<Excavator> producer = new ExcavatorProducer();
+            return (List<T>) producer.get();
+        }
+    }
+
+    @Override
+    public void fill(List<? super T> machines, T value) {
+        for (int i = 0; i < machines.size(); i++) {
+            machines.set(i, value);
+        }
+
+    }
+
+    @Override
+    public void startWorking(List<? extends T> machines) {
+        for (T machine : machines) {
+            machine.doWork();
+        }
+
+    }
+
 }
